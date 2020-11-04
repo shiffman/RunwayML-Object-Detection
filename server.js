234dd706369439ecefd5b166d46834f3b79f1ceb
Glitch (hello-express) <none>
2020-11-04 14:19:43 +0000

    🍩☃️ Checkpoint ./README.md:24668690/1351 ./public/style.css:24668690/1354 ./public/index.html:24668690/412 ./server.js:24668690/2164

--- server.js
+++ server.js
@@ -1,34 +1,26 @@
-// server.js
-// where your node app starts
 
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
+const { HostedModel } = require('@runwayml/hosted-models');
 const express = require("express");
 const app = express();
 
-// our default array of dreams
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
-
 // make all the files in 'public' available
-// https://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
-
-// https://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
-});
-
-// send the default array of dreams to the webpage
-app.get("/dreams", (request, response) => {
-  // express helps us take JS objects and send them as JSON
-  response.json(dreams);
-});
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
+
+
+const model = new HostedModel({
+  url: "https://skygan-70dc7f3a.hosted-models.runwayml.cloud/v1/",
+  token: "32H27Z8P299CYIs+p9xayA==",
+});
+
+
+app.post('/runwayml', async (request, response) => {
+  const inputs = request.body;
+  const outputs = await model.query(data);
+  response.json(outputs);
+});
+
